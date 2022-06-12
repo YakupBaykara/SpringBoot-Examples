7de93aa040d7185158f99240f1ae0fe547c02a6e
unknown <baykarayakup44@hotmail.com>
2022-06-12 22:52:00 +0300

    logging example

---
+++ 20.logging/src/main/java/com/logging/LogApp.java
@@ -0,0 +1,13 @@
+package com.logging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogApp.class, args);
+	}
+
+}
